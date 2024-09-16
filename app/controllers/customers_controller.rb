0befb427bb3a3bb0cfc100669e65135fc3295de1
pickles0417 <tkm2933@icloud.com>
2024-09-16 18:44:22 +0900

    コントローラーとルーティング設定
    edit/update/destroy

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
   def index
     @customer = Customer.all.order("created_at DESC")
   end
@@ -18,16 +19,21 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @customer = Customer.find(params[:id])
   end
 
   def edit
-    @customer = Customer.find(params[:id])
   end
 
   def update
-    customer = Customer.find(params[:id])
-    customer.update(customer_params)
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @customer.destroy
     redirect_to root_path
   end
 
@@ -35,5 +41,9 @@ class CustomersController < ApplicationController
 
   def customer_params
     params.require(:customer).permit(:customer_name, :category_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :capacity, :contract_id, :remarks_column)
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "customers#index"
-  resources :customers, only: [:index, :new, :create, :show, :edit, :update]
+  resources :customers do
+  end
 end
