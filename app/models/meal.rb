11b1f8aba84032ca308cae1b8f0e11466a1c9b81
pickles0417 <tkm2933@icloud.com>
2024-09-17 17:07:54 +0900

    activehash編集

---
+++ app/models/meal.rb
@@ -0,0 +1,10 @@
+class Meal < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' }, { id: 1, name: 'クックフリーズ' }, { id: 2, name: 'クックチル' },
+    { id: 3, name: 'クックサーブ' }, { id: 4, name: '業務委託' }, { id: 5, name: 'お弁当' },
+    { id: 6, name: '手作り' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :dailies
+end
