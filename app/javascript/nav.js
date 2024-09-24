b649e945467f66fb2aeca35d9c2574f3332d814f
pickles0417 <tkm2933@icloud.com>
2024-09-24 18:37:06 +0900

    プルダウンメニュー作成

---
+++ app/javascript/nav.js
@@ -0,0 +1,39 @@
+window.addEventListener('turbo:load', function(){
+  const menu = document.getElementById("menu1");
+  const pullDownMenu = document.getElementById("menu1-pull-down");
+
+  menu.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color: #5bc8ac;");
+  });
+  menu.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color: #5bc8ac;");
+  });
+
+  menu.addEventListener('click', function() {
+    if (pullDownMenu.getAttribute("style") == "display:block;") {
+      pullDownMenu.removeAttribute("style", "display:block;");
+    } else {
+      pullDownMenu.setAttribute("style", "display:block;");
+    }
+  });
+});
+
+window.addEventListener('turbo:load', function(){
+  const menu = document.getElementById("menu2");
+  const pullDownMenu = document.getElementById("menu2-pull-down");
+
+  menu.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color: #5bc8ac;");
+  });
+  menu.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color: #5bc8ac;");
+  });
+
+  menu.addEventListener('click', function() {
+    if (pullDownMenu.getAttribute("style") == "display:block;") {
+      pullDownMenu.removeAttribute("style", "display:block;");
+    } else {
+      pullDownMenu.setAttribute("style", "display:block;");
+    }
+  });
+});
