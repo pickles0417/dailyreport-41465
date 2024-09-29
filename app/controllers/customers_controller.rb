2e981b9b6a4b085585e3a2cedafcdea2558be397
pickles0417 <tkm2933@icloud.com>
2024-09-29 10:12:36 +0900

    全体修正

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy, :new]
+  before_action :authenticate_user!, except: [:index]
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
   def index
     @customer = Customer.includes(:user).order('created_at DESC')
